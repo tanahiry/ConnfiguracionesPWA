f557b69b40f6bd7993bda9bf79e399c0ed80199d
tanahiry <cinthiavitia18@gmail.com>
2024-03-03 17:26:51 -0800

    Actividad 8, Configuraciones PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'v1_cache_entrenamientos_personalizados',
   urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
